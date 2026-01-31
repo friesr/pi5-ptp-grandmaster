49304c2394cd8682310ec6fa3aeff2c304c49fa6
friesr <friesr@yahoo.com>
2026-01-31 06:56:48 -0700

    Update system_validation.py

--- backend/validation/system_validation.py
+++ backend/validation/system_validation.py
@@ -9,3 +9,24 @@ def validate_ptp(ptp):
     if offset > 200:
         return "yellow"
     return "green"
+
+def validate_system_health(system):
+    if system["memory"] > 90:
+        return "red"
+    if system["memory"] > 75:
+        return "yellow"
+
+    load1 = system["cpu_load"][0]
+    if load1 > 4.0:
+        return "red"
+    if load1 > 2.0:
+        return "yellow"
+
+    return "green"
+
+def validate_storage(local_percent, nas_percent):
+    if local_percent > 95 or nas_percent > 95:
+        return "red"
+    if local_percent > 80 or nas_percent > 80:
+        return "yellow"
+    return "green"
