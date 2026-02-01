d6158bc5ab49782076fe96fdfb09497ac4e7fb99
friesr <friesr@yahoo.com>
2026-01-31 21:23:21 -0700

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,36 @@ const routes = [
     path: "/",
     name: "home",
     component: () => import("../components/GlobalSystemGrid.vue")
+  },
+  {
+    path: "/map",
+    name: "map",
+    component: () => import("../pages/GlobalMap.vue")
+  },
+  {
+    path: "/replay",
+    name: "replay",
+    component: () => import("../pages/GlobalReplay.vue")
+  },
+  {
+    path: "/archive",
+    name: "archive",
+    component: () => import("../pages/GlobalArchive.vue")
+  },
+  {
+    path: "/storyboards",
+    name: "storyboards",
+    component: () => import("../pages/GlobalStoryboards.vue")
+  },
+  {
+    path: "/control-room",
+    name: "control-room",
+    component: () => import("../pages/GlobalControlRoom.vue")
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: () => import("../components/GlobalSystemGrid.vue")
   }
 ]
 
