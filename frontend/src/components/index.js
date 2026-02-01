5fb2e50ba81c14e62efa73ac1429038a31bd9e00
friesr <friesr@yahoo.com>
2026-01-31 20:21:33 -0700

    Create index.js

---
+++ frontend/src/components/index.js
@@ -0,0 +1,31 @@
+// Global UI Component Index
+// Centralized export surface for all observatory components
+
+// Constellation
+export { default as GlobalConstellationPanel } from "./GlobalConstellationPanel.vue"
+export { default as GlobalConstellationMap } from "./GlobalConstellationMap.vue"
+export { default as GlobalConstellationHealth } from "./GlobalConstellationHealth.vue"
+export { default as GlobalConstellationSummaryCard } from "./GlobalConstellationSummaryCard.vue"
+export { default as GlobalConstellationGrid } from "./GlobalConstellationGrid.vue"
+
+// Incidents
+export { default as GlobalIncidentCard } from "./GlobalIncidentCard.vue"
+export { default as GlobalIncidentList } from "./GlobalIncidentList.vue"
+
+// Antenna
+export { default as GlobalAntennaPanel } from "./GlobalAntennaPanel.vue"
+
+// Receiver
+export { default as GlobalReceiverPanel } from "./GlobalReceiverPanel.vue"
+export { default as GlobalReceiverHealthCard } from "./GlobalReceiverHealthCard.vue"
+export { default as GlobalReceiverList } from "./GlobalReceiverList.vue"
+
+// Environment / Interference / Multipath
+export { default as GlobalEnvironmentPanel } from "./GlobalEnvironmentPanel.vue"
+export { default as GlobalInterferencePanel } from "./GlobalInterferencePanel.vue"
+export { default as GlobalMultipathPanel } from "./GlobalMultipathPanel.vue"
+
+// System
+export { default as GlobalSystemHealth } from "./GlobalSystemHealth.vue"
+export { default as GlobalSystemSummaryCard } from "./GlobalSystemSummaryCard.vue"
+export { default as GlobalSystemGrid } from "./GlobalSystemGrid.vue"
